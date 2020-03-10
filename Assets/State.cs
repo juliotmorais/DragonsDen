1a1a37342beb4ccaed7397dc5ee1b28f0ddfa707
juliotmorais <juliotmorais@gmail.com>
2020-03-11 00:52:17 +0900

    first commit

---
+++ Assets/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)] [SerializeField] string storyText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
